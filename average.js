82fa1ceb02b9101bd3b23246e92c62b0b8be87b9
Aarush2k1 <56411093+Aarush2k1@users.noreply.github.com>
2021-12-19 22:47:43 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  if(numbers.lenggth!=Nan){
+    return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  }else return Nan;
 }
 
 module.exports = {average};
